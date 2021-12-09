70e0b18bffd229412ad753cbda50f96053d1f8f0
McNerdius <mcnerd@techie.com>
2021-12-09 09:47:06 -0700

    build with correct action output (string.Join)

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
-# dotnet publish Action/MarkDigger.csproj -c Release -o Action/win-x64/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish Action/MarkDigger.csproj -c Release -o Action/win-x64/ -r win-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
 
-# if ($LASTEXITCODE -ne 0) {
-#     exit $LASTEXITCODE
-# }
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
 
 dotnet publish Action/MarkDigger.csproj -c Release -o Action/linux-x64/ -r linux-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
 
@@ -10,8 +10,8 @@ if ($LASTEXITCODE -ne 0) {
     exit $LASTEXITCODE
 }
 
-# dotnet publish Action/MarkDigger.csproj -c Release -o Action/osx-x64/ -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish Action/MarkDigger.csproj -c Release -o Action/osx-x64/ -r osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true --self-contained true /p:DebugType=None /p:IncludeNativeLibrariesForSelfExtract=true
 
-# if ($LASTEXITCODE -ne 0) {
-#     exit $LASTEXITCODE
-# }
+if ($LASTEXITCODE -ne 0) {
+    exit $LASTEXITCODE
+}
